586a64c2fa32ab8f500f564e8b5c4d522f15b370
DmiDro <dmitrii_dron@yahoo.com>
2025-04-17 01:04:08 +0300

    Update status.py

--- test_bot_full/db/status.py
+++ test_bot_full/db/status.py
@@ -1,7 +1,7 @@
 # test_bot_full/db/status.py
 
 import psycopg2
-from test_bot_full.db.write import get_connection
+from db.write import get_connection  # ✅ заменили
 
 async def is_first_launch(user_id: int) -> bool:
     print(f"[DEBUG] Проверяем первый запуск для user_id = {user_id}")
