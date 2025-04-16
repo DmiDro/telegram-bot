438e1a3984eefd7d730d9b011a1712f09ccb6911
DmiDro <dmitrii_dron@yahoo.com>
2025-04-17 01:15:30 +0300

    Update init.sql

--- test_bot_full/db/init.sql
+++ test_bot_full/db/init.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS users_results (
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL UNIQUE,
-    full_name TEXT,
-    subscribe BOOLEAN DEFAULT TRUE,
-    first_test_date DATE,
-    first_test_time TIME,
-    forecast_time TIME DEFAULT '09:01:00',
+    id               SERIAL PRIMARY KEY,
+    user_id          BIGINT       NOT NULL UNIQUE,
+    full_name        TEXT,
+    subscribe        BOOLEAN      DEFAULT TRUE,
+    first_test_date  DATE,
+    first_test_time  TIME,
+    forecast_time    TIME         DEFAULT '09:01:00',
     unsubscribe_date DATE,
 
-    archetype_key TEXT,
-    maturity_key TEXT,
-    socionics_key TEXT,
-    character_key TEXT
+    archetype_key    TEXT,
+    maturity_key     TEXT,
+    socionics_key    TEXT,
+    character_key    TEXT
 );
