1c30e1d37434270124e96f73342a6d59b60d2119
DmiDro <dmitrii_dron@yahoo.com>
2025-04-17 16:00:10 +0300

    Update sender.py

--- test_bot_full/schedule/sender.py
+++ test_bot_full/schedule/sender.py
@@ -23,7 +23,7 @@ def setup_scheduler(bot: Bot):
     scheduler = AsyncIOScheduler(timezone=tz_istanbul)
 
     @scheduler.scheduled_job(
-        CronTrigger(hour=14, minute=59, timezone=tz_istanbul)  # Явно указываем таймзону!
+        CronTrigger(hour=16, minute=5, timezone=tz_istanbul)  # Явно указываем таймзону!
     )
     async def send_recommendations():
         now = datetime.now(tz_istanbul).strftime("%Y-%m-%d %H:%M:%S")
