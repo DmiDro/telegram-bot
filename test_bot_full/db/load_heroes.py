2ca7f424a964b1af2691887a4a2d123efe74fd6b
DmiDro <dmitrii_dron@yahoo.com>
2025-04-17 03:10:32 +0300

    Update load_heroes.py

--- test_bot_full/db/load_heroes.py
+++ test_bot_full/db/load_heroes.py
@@ -4,24 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_PARAMS = {
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT"),
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 async def get_heroes():
-    conn = await asyncpg.connect(**DB_PARAMS)
-    rows = await conn.fetch("SELECT name, description, link FROM heroes")
-    await conn.close()
-
-    return [
-        {
-            "name": row["name"],
-            "description": row["description"],
-            "link": row["link"]
-        }
-        for row in rows
-    ]
+    conn = None
+    try:
+        conn = await asyncpg.connect(DATABASE_URL)
+        rows = await conn.fetch("SELECT name, description, link FROM heroes")
+        return [
+            {
+                "name": row["name"],
+                "description": row["description"],
+                "link": row["link"]
+            }
+            for row in rows
+        ]
+    except Exception as e:
+        print(f"❌ Ошибка при загрузке героев: {e}")
+        return []
+    finally:
+        if conn:
+            await conn.close()
